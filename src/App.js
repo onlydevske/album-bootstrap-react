f103e7e823f3e536548e31b7f7961f4dc0a2b874
ChrisAchinga <achinga.chris@gmail.com>
2020-11-28 23:08:50 +0300

    src:bootstrap:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container } from 'react-bootstrap'
 
-function App() {
+import './App.css'
+import Menu from './components/Menu'
+import Header from './components/Header'
+import Album from './components/Album'
+import Footer from './components/Footer'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Menu />
+      <main>
+        <Container>
+          <Header />
+        </Container>
+
+        <Container>
+          <Album />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  )
 }
 
-export default App;
+export default App
