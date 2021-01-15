1a03e449ad1032a8f487fbb6efeafaecb9e3350e
ChrisAchinga <achinga.chris@gmail.com>
2021-01-15 21:51:24 +0300

    data folder created

---
+++ src/data/dummyData.js
@@ -0,0 +1,58 @@
+const dummyData = [
+  {
+    id: 1,
+    title: 'card titles',
+    text: 'a brief description of the image/video group',
+    editTime: 9,
+  },
+  {
+    id: 2,
+    title: 'card titles',
+    text: 'a brief description of the image/video group',
+    editTime: 3,
+  },
+  {
+    id: 3,
+    title: 'card titles',
+    text: 'a brief description of the image/video group',
+    editTime: 2,
+  },
+  {
+    id: 4,
+    title: 'card titles',
+    text: 'a brief description of the image/video group',
+    editTime: 9,
+  },
+  {
+    id: 5,
+    title: 'card titles',
+    text: 'a brief description of the image/video group',
+    editTime: 6,
+  },
+  {
+    id: 6,
+    title: 'card titles',
+    text: 'a brief description of the image/video group',
+    editTime: 7,
+  },
+  {
+    id: 7,
+    title: 'card titles',
+    text: 'a brief description of the image/video group',
+    editTime: 9,
+  },
+  {
+    id: 8,
+    title: 'card titles',
+    text: 'a brief description of the image/video group',
+    editTime: 9,
+  },
+  {
+    id: 9,
+    title: 'card titles',
+    text: 'a brief description of the image/video group',
+    editTime: 9,
+  },
+]
+
+module.exports = dummyData
